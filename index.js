06bd58dc3b8114ae7fada134520dd1e4690dfa85
gauravappy97 <gauravappy1997@gmail.com>
2020-09-02 23:53:20 +0530

    added js basics&2-projects

---
+++ index.js
@@ -0,0 +1,42 @@
+// 1. ways to print in javaScript
+// console.log("Hello World");
+// alert("me");
+// document.write("this is document write");
+
+// 2. javaScript console API
+// console.log('Hello world',4+6,'Another log');
+// console.warn('This is warning');
+// console.error('This is an error');
+
+// 3. javaScript Variables
+// what is variable? - Container to store data values.
+
+//4. Data types in JavaScript
+//numbers
+var number1 = 5;
+var number2 = 6;
+// console.log(number1+number2);
+
+//strings
+var str1 = "Gaurav"
+var str2 = "Kumar"
+// console.log(str1+str2);
+
+//objects
+var marks = {
+    gaurav: 80,
+    rajat: 90,
+    Prabal: 85,
+    Ritesh: 80,
+    Shubham: 80
+}
+// console.log(marks)
+
+//array
+myarr = [1, 2, 3, 4, 'Gaurav']
+// console.log(myarr)
+
+//dates in javaScript
+let mydate = new Date()
+// console.log(mydate.getTime());
+// console.log(mydate.getFullYear());
